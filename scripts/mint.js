5c2931dbef019c90e3f51fdc33c689363a6786b0
Akshar Prajapati <aksharpp369@gmail.com>
2022-05-18 11:27:13 +0530

    second part of minting NFT

---
+++ scripts/mint.js
@@ -0,0 +1,14 @@
+//creating a mint task to actually call our smart contract!
+
+const { task } = require("hardhat/config");
+const { getContract } = require("./helpers");
+
+task("mint", "Mints from the NFT contract")
+  .addParam("address", "The address to receive a token")
+  .setAction(async function (taskArguments, hre) {
+    const contract = await getContract("NFT", hre);
+    const transactionResponse = await contract.mintTo(taskArguments.address, {
+      gasLimit: 500_000,
+    });
+    console.log(`Transaction Hash: ${transactionResponse.hash}`);
+  });
