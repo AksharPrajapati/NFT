0dcf007d4e2f69ef3a9ebffc2626d8eaaf3e8f63
Akshar Prajapati <aksharpp369@gmail.com>
2022-05-19 11:32:07 +0530

    verify smart contract

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -3,9 +3,16 @@ pragma solidity ^0.8.0; //version of solidity
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol"; // various controls of ERC721
 import "@openzeppelin/contracts/utils/Counters.sol"; // keep records of total tokens
+import "@openzeppelin/contracts/security/PullPayment.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFT is ERC721 { //inherit NFT from ERC721
+contract NFT is ERC721, PullPayment, Ownable { //inherit NFT from ERC721
     using Counters for Counters.Counter; // track record of minted contracts
+
+    // Constants
+    uint256 public constant TOTAL_SUPPLY = 10_000;
+    uint256 public constant MINT_PRICE = 0.08 ether;
+
     Counters.Counter private currentTokenId;
 
     /// @dev Base token URI used as a prefix by tokenURI().
@@ -15,10 +22,12 @@ contract NFT is ERC721 { //inherit NFT from ERC721
         baseTokenURI = "";
     }
     
-    function mintTo(address recipient)
-        public
-        returns (uint256)
-    {
+    function mintTo(address recipient) public payable returns (uint256) {
+
+        uint256 tokenId = currentTokenId.current();
+        require(tokenId < TOTAL_SUPPLY, "Max supply reached");
+        require(msg.value == MINT_PRICE, "Transaction value did not equal the mint price");
+
         currentTokenId.increment();
         uint256 newItemId = currentTokenId.current();
         _safeMint(recipient, newItemId);
@@ -31,7 +40,12 @@ contract NFT is ERC721 { //inherit NFT from ERC721
     }
 
     /// @dev Sets the base token URI prefix.
-    function setBaseTokenURI(string memory _baseTokenURI) public {
+    function setBaseTokenURI(string memory _baseTokenURI) public onlyOwner {
       baseTokenURI = _baseTokenURI;
     }
+
+    /// @dev Overridden in order to make it an onlyOwner function
+    function withdrawPayments(address payable payee) public override onlyOwner virtual {
+    super.withdrawPayments(payee);
+  }
 }
